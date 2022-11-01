af067700e80e613ab26d85a2d5ec12cd4a25b896
Qois-69912 <qois.firosi@gmail.com>
2022-11-01 14:28:34 +0700

    Use time as volatile parameter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use chrono::*;
+
 fn main() {
+    // Chrono
+    let lc: DateTime<Local> = Local::now();
+
     // LCG
     let c: i64 = 1013904223;
     let m: i64 = 4294967296;
     let a: i64 = 1664525;
-    let x0: i64 = 100;
+    let x0: i64 = (lc.hour() as i64 * lc.minute() as i64 * lc.second() as i64)
+        + lc.day() as i64 * lc.year() as i64 * lc.month() as i64;
     let x1: i64 = ((a * x0) + c) % m;
 
     println!("{}", x1)
