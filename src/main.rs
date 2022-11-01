3a914de5111d924b31819e119087b739c00a6247
Qois-69912 <qois.firosi@gmail.com>
2022-11-01 13:53:37 +0700

    LCG sederhana

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // LCG
+    let c: i32 = 10000;
+    let m: i32 = 90;
+    let a: i32 = 9;
+    let x0: i32 = 100;
+    let x1: i32 = ((a * x0) + c) % m;
+
+    println!("{}", x1)
 }
