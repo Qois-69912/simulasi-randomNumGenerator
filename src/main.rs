23e22295d1ddd83ef71642faa4d42a09052f6611
Qois-69912 <qois.firosi@gmail.com>
2022-11-01 22:18:13 +0700

    Generate random numbers within a range

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
 use chrono::*;
+use std::io;
+use std::io::Write;
+
+// Read User Input and make sure it is number
+fn usr_int(msg: String) -> i64 {
+    let int: i64;
+
+    loop {
+        print!("{}", msg);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error getting guess");
+
+        int = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        break;
+    }
+
+    return int;
+}
 
 fn main() {
     // Chrono
@@ -12,5 +39,29 @@ fn main() {
         + lc.day() as i64 * lc.year() as i64 * lc.month() as i64;
     let x1: i64 = ((a * x0) + c) % m;
 
-    println!("{}", x1)
+    // User Input
+    let awal: i64;
+    let akhir: i64;
+
+    println!("==== NUMBER GENERATOR ==== \n");
+    println!("Generate angka");
+    awal = usr_int("Antara angka : ".to_string());
+    akhir = usr_int("Dan angka : ".to_string());
+
+    // Generate random numbers within a range
+    let offset: i64;
+    let range: i64;
+
+    if awal > akhir {
+        offset = akhir;
+        range = awal - akhir;
+    } else {
+        offset = awal;
+        range = akhir - awal;
+    }
+
+    let result: i64 = offset + (x1 % range);
+
+    // Resultttt
+    println!("Angka acak {} dan {} adalah : {}", awal, akhir, result);
 }
